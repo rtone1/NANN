490153383384fdeabd68e68225b442ac19410ab0
Rodrigo Torres <rtdesigns@yahoo.com>
2015-10-22 11:13:24 -0500

    top navigation functionality done

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,27 @@
+$(function(){
+
+  $.fn.searchMore = function(counter,target,action){
+
+    $(target).on('click', function(){
+      if (counter === 0 ){
+        $(action).addClass('expand');
+        $('.searchIcon').addClass('searchActive');
+        $(action).removeClass('unexpand');
+        counter += 1;
+      } else{
+        $(action).addClass('unexpand');
+        $(action).removeClass('expand');
+        $('.searchIcon').removeClass('searchActive');
+        counter -= 1;
+      }
+    });
+  };
+
+}());
+
 $(document).ready(function(){
   console.log('Hey I\'m loaded and ready to rumble');
+
+  $.fn.searchMore( 0, $('.searchIcon'), $('.textSearch') );
+
 }); // END OF DOCUMENT READY
