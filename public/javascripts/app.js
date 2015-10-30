6311533eeb2bf64de5c124d13e38e58c8d76ab7c
Rodrigo Torres <rtdesigns@yahoo.com>
2015-10-30 11:39:35 -0500

    clean up app.js code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,22 @@
 $(function(){
+  var clockWise = function(){
+    $('.searchIcon').addClass('searchActive');
+    $('.searchIcon').removeClass('unActivate');
+  };
+  var counterClockWise = function(){
+    $('.searchIcon').removeClass('searchActive');
+    $('.searchIcon').addClass('unActivate');
+  };
 
   $.fn.searchMore = function(counter,target,action){
     $(target).on('click', function(){
       if (counter === 0 ){
-        // $(action).addClass('expand');
         $(action).animate({width: 250},200,"linear");
-        // $(action).removeClass('unexpand');
-        $('.searchIcon').addClass('searchActive');
-        $('.searchIcon').removeClass('unActivate');
+        clockWise();
         counter += 1;
       } else{
-        // $(action).addClass('unexpand');
         $(action).animate({width: 45},200,"linear");
-        // $(action).removeClass('expand');
-        $('.searchIcon').removeClass('searchActive');
-        $('.searchIcon').addClass('unActivate');
+        counterClockWise();
         counter -= 1;
       }
     });
@@ -23,8 +25,8 @@ $(function(){
 }());
 
 $(document).ready(function(){
-  // console.log('Hey I\'m loaded and ready to rumble');
 
+  // CALL FUNTIONS ON DOM
   $.fn.searchMore( 0, $('.searchIcon'), $('.textSearch') );
 
 }); // END OF DOCUMENT READY
