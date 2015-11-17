ed29a24f13052d35d68b3790a0a1af2769c58c37
Rodrigo Torres <rtdesigns@yahoo.com>
2015-11-17 13:36:50 -0600

    bugs worked out and animated image zoom completed

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,13 +11,26 @@ $(function(){
   $.fn.searchMore = function(counter,target,action){
     $(target).on('click', function(){
       if (counter === 0 ){
-        $(action).animate({width: 250},200,"linear").css({opacity: 1});
+        if ($(window).width() > 1600){
+          $(action).animate({width: 550},200,"linear").css({opacity: 1});
+        }
+        if ($(window).width() < 1600){
+          $(action).animate({width: 250},200,"linear").css({opacity: 1});
+        }
+        $('.textSearch').focus();
         clockWise();
         counter += 1;
       } else{
-        $(action).animate({width: 45},200,"linear",function() {
-          $(action).css({opacity: 0});
-        });
+        if ($(window).width() > 1600){
+          $(action).animate({width: 80},200,"linear",function() {
+            $(action).css({opacity: 0});
+          });
+        }
+        if ($(window).width() < 1600){
+          $(action).animate({width: 45},200,"linear",function() {
+            $(action).css({opacity: 0});
+          });
+        }
         counterClockWise();
         counter -= 1;
       }
@@ -34,7 +47,30 @@ $(function(){
       if(counter === 1 && $(window).width() > 650){
         $(action).css({width: 250, opacity: 1});
       }
+      if(counter === 1 && $(window).width() > 1600){
+        $(action).css({width: 550, opacity: 1});
+      }
     });
+
+  };
+
+  $.fn.zoomImage = function(counter){
+      if($(window).width() < 650){
+        $('.art-ct img').on('click', function(){
+          if(counter === 0){
+            counter += 1;
+            var that = this;
+              $(that).css({ position: "fixed", top: 25 + '%', width: 90+ '%', margin: 0, left: -300, zIndex: 1000 }).animate({left: 5 + '%'}, 200, 'linear');
+              $('.back').addClass('black')
+          $('.back').on('click', function(){
+            $(that).removeAttr('style');
+            $('.back').removeClass('black');
+            counter = 0;
+          });
+          }
+        });
+      };
+
 
   };
 
@@ -44,4 +80,5 @@ $(document).ready(function(){
 
   // CALL FUNTIONS ON DOM
   $.fn.searchMore( 0, $('.searchIcon'), $('.textSearch') );
+  $.fn.zoomImage(0);
 }); // END OF DOCUMENT READY
