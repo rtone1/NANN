9a29765a34eb54adeb96fc0b302db1e351f4d3b5
Rodrigo Torres <rtdesigns@yahoo.com>
2015-11-23 15:39:01 -0600

    consolidated styles and fixed minor bugs on js file

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,18 +18,15 @@ $(function(){
   var windowResize = function( action ){
     $(window).resize(function(){
       if($(window).width() < 655){
-        $('.searchForm').css({ width: 90 + '%'});
-        $('.textSearch').css({ width: 100 + '%', opacity: 1 });
+        $('.textSearch').removeAttr('style');
         $('.ctaLarge').last().show();
       } else if ($(window).width() > 655) {
-        $('.searchForm').removeAttr('style');
-        $('.textSearch').removeAttr('style');
         $('.ctaLarge').last().hide();
       }
       if(counter === 1 && $(window).width() > 655){
         $(action).css({width: 250, opacity: 1});
       }
-      if(counter === 1 && $(window).width() > 1600){
+      if(counter === 1 && $(window).width() > 1599){
         $(action).css({width: 400, opacity: 1});
       }
     });
@@ -39,22 +36,22 @@ $(function(){
   $.fn.searchMore = function( target, action ){
     $(target).on('click', function(){
       if (counter === 0 ){
-        if ($(window).width() > 1600){
+        if ($(window).width() > 1599){
           $(action).animate({width: 400},200,"linear").css({opacity: 1});
         }
-        if ($(window).width() < 1600){
+        if ($(window).width() < 1599){
           $(action).animate({width: 250},200,"linear").css({opacity: 1});
         }
         $(action).focus();
         clockWise();
         counter += 1;
       } else{
-        if ($(window).width() > 1600){
+        if ($(window).width() > 1599){
           $(action).animate({width: 80},200,"linear",function() {
             $(action).css({opacity: 0});
           });
         }
-        if ($(window).width() < 1600){
+        if ($(window).width() < 1599){
           $(action).animate({width: 45},200,"linear",function() {
             $(action).css({opacity: 0});
           });
@@ -68,13 +65,17 @@ $(function(){
 
   // ZOOM IMAGE FUNCTION //
   $.fn.zoomImage = function( counter ){
+    var that;
     if($(window).width() < 650){
       $('.art-ct img').on('click', function(){
         if(counter === 0){
           counter += 1;
-          var that = this;
-            $(that).css({ position: "fixed", top: 25 + '%', width: 90+ '%', margin: 0, left: -300, zIndex: 1000 }).animate({left: 5 + '%'}, 200, 'linear');
+          that = this;
+            $(that).css({ position: "fixed", top: 50 + "%", transform: 'translate(0%, -50%)', width: 90 + '%', margin: 0, left: -300, zIndex: 1000 }).animate({left: 5 + '%'}, 200, 'linear');
             $('.back').addClass('black');
+            if($(window).width() > 450){
+              $(that).css({ position: "fixed", top: 50 + "%", transform: 'translate(0%, -50%)', width: 70 + '%', margin: 0, left: -300, zIndex: 1000 }).animate({left: 15 + '%'}, 100);
+            }
         $('.back').on('click', function(){
           $(that).removeAttr('style');
           $('.back').removeClass('black');
@@ -82,7 +83,15 @@ $(function(){
         });
         }
       });
-    };
+      $(window).resize(function(){
+        if($(window).width() > 450){
+          $(that).css({ position: "fixed", top: 50 + "%", transform: 'translate(0%, -50%)', width: 70 + '%', margin: 0, left: -300, zIndex: 1000, left: 15 + '%' });
+        } else {
+          $(that).css({ position: "fixed", top: 50 + "%", transform: 'translate(0%, -50%)', width: 90 + '%', margin: 0, left: -300, zIndex: 1000, left: 5 + '%' });
+        }
+        if(counter === 0){ $(that).removeAttr('style'); }
+      });
+    }
   };
 
   // MMENU DISPLAY ON MOBILE FUNCTION //
